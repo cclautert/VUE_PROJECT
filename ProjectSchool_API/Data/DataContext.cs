ecfea1ca20e8f8b8ecda65a0f63b1ecbb773e1fe
cristiano.c.lautert@gmail.com <cristiano.c.lautert@gmail.com>
2020-01-14 12:44:31 -0300

    [CHANGE] Aula 74 Criando Contexto

--- ProjectSchool_API/Data/DataContext.cs
+++ ProjectSchool_API/Data/DataContext.cs
@@ -1,8 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using ProjectSchool_API.Models;
+
 namespace ProjectSchool_API.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Aluno> Alunos { get; set;}
+        public DbSet<Professor> Professores { get; set; }
     }
 }
